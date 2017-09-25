ad184f1ee1aa3c303e53fbafac204fded34caa9c
justiceadamsUNI <adamsjal@uni.edu>
2017-09-24 21:22:13 -0500

    added parser class and enum  for stack values

---
+++ Compiler/src/implementation/Parser.cpp
@@ -0,0 +1,98 @@
+#include "../header/Parser.h"
+#include <algorithm>
+
+bool Parser::isTerminalValue(StackValues value)
+{
+	list<StackValues>::iterator foundElement = std::find(TerminalValues.begin(), TerminalValues.end(), value);
+	return foundElement != TerminalValues.end();
+}
+
+Parser::StackValues Parser::mapFromScannerTokenToStackValue(Token InToken)
+{
+	switch (InToken.getTokenType())
+	{
+	case INTEGER:
+		return INTEGER_LITERAL;
+	case DATA_TYPE:
+		return InToken.getValue() == "integer" ? INTEGER_DATA_TYPE : BOOLEN_DATA_TYPE;
+	case BOOLEAN:
+		return BOOLEAN_LITERAL;
+	case ARITHMETIC_OPERATOR:
+		if (InToken.getValue() == "+")
+		{
+			return PLUS_OPERATOR;
+		}
+		else if (InToken.getValue() == "-")
+		{
+			return MINUS_OPERATOR;
+		}
+		else if (InToken.getValue() == "/")
+		{
+			return DIVIDES_OPERATOR;
+		}
+		else
+		{
+			return MULTIPLY_OPERATOR;
+		};
+	case PRIMITIVE_KEYWORD:
+		return FUNCTION;
+	case PARENTHESIS:
+		return InToken.getValue() == "(" ? LEFT_PAREN : RIGHT_PAREN;
+	case COMMA:
+		return COMMA_LITERAL;
+	case COLON:
+		return COLON_LITERAL;
+	case IDENTIFIER:
+		if (InToken.getValue() == "main")
+		{
+			return MAIN;
+		}
+		else if (InToken.getValue() == "print")
+		{
+			return PRINT;
+		}
+		else
+		{
+			return IDENTIFIER;
+		};
+	case COMPARATOR:
+		if (InToken.getValue() == "<")
+		{
+			return LESS_THAN_OPERATOR;
+		}
+		else
+		{
+			return EQUAL_SIGN;
+		};
+	case LOGICIAL_OPERATOR:
+		if (InToken.getValue() == "and")
+		{
+			return AND;
+		}
+		else if (InToken.getValue() == "not")
+		{
+			return NOT;
+		}
+		else
+		{
+			return OR;
+		};
+	case CONDITIONAL:
+		if (InToken.getValue() == "if")
+		{
+			return IF;
+		}
+		else if (InToken.getValue() == "then")
+		{
+			return THEN;
+		}
+		else
+		{
+			return ELSE;
+		};
+	case END_OF_FILE:
+		return END_OF_STREAM;
+	default:
+		break;
+	}
+}
---
+++ Compiler/test/ParserTest.cpp
@@ -0,0 +1,4 @@
+#include "catch.hpp"
+#include"../src/header/Parser.h"
+
+// Need to fill out once we actually have a public method for our Parser API.
