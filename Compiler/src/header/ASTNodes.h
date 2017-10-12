97e4951c56be8c97c527c3be87c16b11e640a169
hilliardj <31747662+hilliardj@users.noreply.github.com>
2017-10-11 21:32:15 -0500

    Getters done
    Submittedl

--- Compiler/src/header/ASTNodes.h
+++ Compiler/src/header/ASTNodes.h
@@ -5,187 +5,150 @@
 using namespace std;
 
 class ASTNode {
-	virtual void polymorphicEnabler() {}
-	// Base AST Node from which every class extends.
-};
-
-class IdentifierNode : public ASTNode {
 public:
+	ASTNode(ASTNodeType InputNode){
+		NodeType = InputNode;
+	}
+	string getIdentifierName() {
+		return IdentifierName;
+	}
+	ASTNode* getFactorNode() {
+		return FactorNode;
+	}
+	ASTNode* getBaseTermNode() {
+		return BaseTermNode;
+	}
+		ASTNode* getBaseSimpleExprNode() {
+		return BaseSimpleExprNode;
+	}
+	ASTNode* getBaseExprNode(){
+		return BaseExprNode;
+	}
+	string getLiteralValue(){
+		return LiteralValue;
+	}
+	vector<ASTNode*> getExpressions(){
+		return Expressions;
+	}
+	ASTNode* getNonEmptyActualsNode(){
+		return NonEmptyActualsNode;
+	}
+	ASTNode* getLiteralNode(){
+		return LiteralNode;
+	}
+	ASTNode* getIdentifierNode(){
+		return IdentifierNodeVar;
+	}
+	ASTNode* getBaseActualsNode(){
+		return BaseActualsNode;
+
+	}
+	ASTNode* getBaseExprNode2(){
+		return BaseExprNode2;
+	}
+	ASTNode* getBaseExprNode3(){
+		return BaseExprNode3;
+	}
+	ASTNode* getFactorNode2(){
+		return FactorNode2;
+	}
+	ASTNode* getBaseTermNode2(){
+		return BaseTermNode2;
+	}
+	ASTNode* getBaseSimpleExprNode2(){
+		return BaseSimpleExprNode2;
+	}
+	string getDataType(){
+		return DataType;
+	}
+	ASTNode* getIdentifierNode(){
+		return IdentifierNode;
+	}
+	ASTNode* getTypeNode(){
+		return TypeNode;
+	}
+	vector<ASTNode*> getPrintStatements(){
+		return PrintStatements;
+	}
+	vector<ASTNode*> getFormalNodes(){
+		return FormalNodes;
+	}
+	ASTNode* getFormalsNode(){
+		return FormalsNode;
+	}
+	ASTNode* getBodyNode(){
+		return BodyNode;
+	}
+	vector<ASTNode*> getDefNodes(){
+		return DefNodes;
+	}
+	vector<ASTNode*> getDefinitions() {
+		return Definitions;
+	}
+private :
+	ASTNodeType NodeType;
 	string IdentifierName;
-};
-
-class FactorNode : public ASTNode {
-	// Base Factor Node that the others extend from
-	// Should never be on the stack.
-};
-
-class BaseActualsNode : public ASTNode {
-	// Contains nothing.
-	// Can be on the stack.
-};
-
-class BaseTermNode : public ASTNode {
-public:
-	FactorNode* Factor1;
-};
-
-class BaseSimpleExprNode : public ASTNode {
-public:
-	BaseTermNode*  Term1;
-};
-
-class BaseExprNode : public ASTNode {
-public:
-	BaseSimpleExprNode* SimpleExpression1;
-};
-
-class FormalsNode : public ASTNode {
-	// Contains Nothing
-	// Can be on the stack since Formals can be epsilon
-};
-
-class PrintStatementNode : public ASTNode {
-public:
-	BaseExprNode* Expression;
-};
-
-class LiteralNode : public ASTNode {
-public:
+	ASTNode* FactorNode;
+	ASTNode* BaseTermNode;
+	ASTNode* BaseSimpleExprNode;
+	ASTNode* BaseExprNode;
 	string LiteralValue;
-	// This is a base class
-	// Can not be on the stack.
-};
-
-class IntegerLiteralNode : public LiteralNode {
-};
-
-class BooleanLiteralNode : public LiteralNode {
-};
-
-class NonEmptyActualsNode : public ASTNode {
-public:
-	vector<BaseExprNode*> Expressions;
-};
-
-class NonBaseActualsNode : public BaseActualsNode {
-public:
-	NonEmptyActualsNode* NonEmptyActuals;
-};
-
-class ParenExprFactorNode : public FactorNode {
-public:
-	BaseExprNode* Expression;
-};
-
-class SubtractionFactorNode : public FactorNode {
-public:
-	FactorNode* Factor;
-};
-
-class LiteralFactorNode : public FactorNode {
-public:
-	LiteralNode* Literal;
-};
-
-class IdentifierFactorNode : public FactorNode {
-public:
-	IdentifierNode* Identifier;
-	BaseActualsNode* Actuals;
-};
-
-class SingletonIdentifierFactorNode : public FactorNode {
-public:
-	IdentifierNode* Identifier;
-};
-
-class NotFactorNode : public FactorNode {
-public:
-	FactorNode* Factor;
-};
-
-class IfFactorNode : public FactorNode {
-public:
-	BaseExprNode* Expression1;
-	BaseExprNode* Expression2;
-	BaseExprNode* Expression3;
-};
-
-class MultiplicatorTermNode : public BaseTermNode {
-public:
-	FactorNode* Factor2;
-};
-
-class DividerTermNode : public BaseTermNode {
-public:
-	FactorNode* Factor2;
-};
-
-class AndTermNode : public BaseTermNode {
-public:
-	FactorNode* Factor2;
-};
-
-class SubtractorSimpleExprNode : public BaseSimpleExprNode {
-public:
-	BaseTermNode*  Term2;
-};
-
-class OrSimpleExprNode : public BaseSimpleExprNode {
-public:
-	BaseTermNode*  Term2;
-};
-
-class AdditionSimpleExprNode : public BaseSimpleExprNode {
-public:
-	BaseTermNode*  Term2;
-};
-
-class EqualToExprNode : public BaseExprNode {
-public:
-	BaseSimpleExprNode* SimpleExpression2;
-};
-
-class LessThanExprNode : public BaseExprNode {
-public:
-	BaseSimpleExprNode* SimpleExpression2;
-};
-
-class TypeNode : public ASTNode {
-public:
+	vector<ASTNode*> Expressions;
+	ASTNode* NonEmptyActualsNode;
+	ASTNode* LiteralNode;
+	ASTNode* IdentifierNodeVar;
+	ASTNode* BaseActualsNode;
+	ASTNode* BaseExprNode2;
+	ASTNode* BaseExprNode3;
+	ASTNode* FactorNode2;
+	ASTNode* BaseTermNode2;
+	ASTNode* BaseSimpleExprNode2;
 	string DataType;
+	ASTNode* IdentifierNode2;
+	ASTNode* TypeNode;
+	vector<ASTNode*> PrintStatements;
+	vector<ASTNode*> FormalNodes;
+	ASTNode* FormalsNode;
+	ASTNode* BodyNode;
+	vector<ASTNode*> DefNodes;
+	vector<ASTNode*>Definitions;
 };
 
-class BodyNode : public ASTNode {
-public:
-	vector<PrintStatementNode*> PrintStatements;
-	BaseExprNode* Expression;
+enum ASTNodeType {
+	IdentifierNode,
+	FactorNode,
+	BaseActualsNode,
+	BaseTermNode,
+	BaseSimpleExprNode,
+	BaseExprNode,
+	FormalsNode,
+	PrintStatemetNode,
+	LiteralNode,
+	IntegerLiteralNode,
+	BooleanLiteralNode,
+	NonEmptyActualsNode,
+	NonBaseActualsNode,
+	ParenExprFactorNode,
+	SubtractionFactorNode,
+	LiteralFactorNode,
+	IdentifierFactorNode,
+	SingletonIdentifierFactorNode,
+	NotFactorNode,
+	IfFactorNode,
+	MultiplicatorTermNode,
+	DividerTermNode,
+	AndTermNode,
+	SubtractorSimpleExprNode,
+	OrSimpleExprNode,
+	AdditionSimpleExprNode,
+	EqualToExprNode,
+	LessThanExprNode,
+	TypeNode,
+	BodyNode,
+	FormalNode,
+	NonEmptyFormalsNode,
+	DefNode,
+	DefinitionsNode,
+	ProgramNode
 };
 
-class FormalNode : public ASTNode {
-public:
-	IdentifierNode* Identifier;
-	TypeNode* Type;
-};
-
-class NonEmptyFormalsNode : public FormalsNode {
-public:
-	vector<FormalNode*> FormalNodes;
-};
-
-class DefNode : public ASTNode {
-public:
-	IdentifierNode* Identifier;
-	FormalsNode* Formals;
-	TypeNode* Type;
-	BodyNode* Body;
-};
-
-class DefinitionsNode : public ASTNode {
-public:
-	vector<DefNode*> DefNodes;
-};
-
-class ProgramNode : public ASTNode {
-public:
-	vector<DefinitionsNode*> Definitions;
-};
