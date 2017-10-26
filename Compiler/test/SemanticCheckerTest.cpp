a73ab45c7e25851b2d6838f92b151352771b5101
justiceadamsUNI <adamsjal@uni.edu>
2017-10-26 16:12:09 -0500

    added test to make sure semantic checker compiles

---
+++ Compiler/test/SemanticCheckerTest.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+#include "../src/header/Parser.h"
+#include "../src/header/SemanticChecker.h"
+
+TEST_CASE("Semantic Checker Finds No Errors Or Warnings For Valid Program", "[Scanner]") {
+	Scanner Scanner("programs/fractionAddition.kln");
+	Parser Parser(Scanner);
+
+	Parser.parseProgram();
+	ASTNode AST = Parser.getFinalASTNode();
+	SemanticChecker SemanticCheckerVar = SemanticChecker(AST);
+
+	REQUIRE(SemanticCheckerVar.getErrors().empty());
+	REQUIRE(SemanticCheckerVar.getWarnings().empty());
+}
