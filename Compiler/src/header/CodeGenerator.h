02fc2cc60b3516c332103708278b18e9fd70bcd2
Justice <adamsjal@uni.edu>
2017-11-05 12:28:48 -0600

    created initial CodeGenerator header file

---
+++ Compiler/src/header/CodeGenerator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SemanticChecker.h"
+
+class CodeGenerator {
+public:
+	CodeGenerator(ASTNode AstTree, map<string, Function> GeneratedSymbolTable) {
+		AstTree = Tree;
+		SymbolTable = GeneratedSymbolTable;
+	}
+
+private:
+	ASTNode Tree = ASTNode(PROGRAM_NODE_TYPE);
+	map<string, Function> SymbolTable;
+	vector<string> Instructions;
+
+	void writeInstructionsToFile();
+};
